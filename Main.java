874dc16e915c78c0a84fbc76a168c00e7bd643f6
gnanasindhucs24-hub <gnanasindhu.cs24@bmsce.ac.in>
2025-11-05 09:32:05 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+import mypackage.addition;
+public class Main{
+    public static void main(String[] args){
+        addition obj=new addition();
+        int result=obj.add(5,10);
+        System.out.println("sum:"+result);
+    }
+}
